ae9b7e33e9833dedb4ae4513a50d481b98af86ed
hyunsoo730 <whgustn730@gmail.com>
2024-07-16 15:14:58 +0900

    Setting: back-end Settings

---
+++ back-end/src/test/java/com/cupid/qufit/QufitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cupid.qufit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QufitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
