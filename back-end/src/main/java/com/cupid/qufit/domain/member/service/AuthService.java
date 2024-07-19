472379fbdde53a1af0707997ad320b2d6d2acb4a
jhw <612gpdnjs@gmail.com>
2024-07-19 18:05:27 +0900

    #S11P12A209-29 #comment BE/ feat : authservice 회원 가입 요청 추가

--- back-end/src/main/java/com/cupid/qufit/domain/member/service/AuthService.java
+++ back-end/src/main/java/com/cupid/qufit/domain/member/service/AuthService.java
@@ -1,12 +1,15 @@
 package com.cupid.qufit.domain.member.service;
 
+import com.cupid.qufit.domain.member.dto.MemberSignupDTO;
 import com.cupid.qufit.domain.member.dto.PrincipalDetails;
 import com.cupid.qufit.entity.Member;
 
 public interface AuthService {
 
     PrincipalDetails kakaoLogin(String accessToken);
-    
+
+    MemberSignupDTO.response signup(String accessToken, MemberSignupDTO.request memberSignupRequestDTO);
+
     /*
     * * member 객체를 PrincipalDetails DTO로 만드는 생성자
     * */
--- back-end/src/main/java/com/cupid/qufit/domain/member/service/AuthServiceImpl.java
+++ back-end/src/main/java/com/cupid/qufit/domain/member/service/AuthServiceImpl.java
@@ -1,11 +1,20 @@
 package com.cupid.qufit.domain.member.service;
 
+import com.cupid.qufit.domain.member.dto.MemberSignupDTO;
+import com.cupid.qufit.domain.member.dto.MemberSignupDTO.request;
+import com.cupid.qufit.domain.member.dto.MemberSignupDTO.response;
 import com.cupid.qufit.domain.member.dto.PrincipalDetails;
-import com.cupid.qufit.domain.member.repository.MemberRepository;
+import com.cupid.qufit.domain.member.repository.profiles.MemberRepository;
 import com.cupid.qufit.entity.Member;
+import com.cupid.qufit.entity.MemberRole;
 import com.cupid.qufit.entity.MemberStatus;
+import com.cupid.qufit.entity.TypeProfiles;
 import com.cupid.qufit.global.exception.CustomException;
 import com.cupid.qufit.global.exception.ErrorCode;
+import java.util.Map;
+import org.springframework.transaction.annotation.Transactional;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.LinkedHashMap;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
@@ -22,9 +31,11 @@ import org.springframework.web.util.UriComponentsBuilder;
 @Service
 @RequiredArgsConstructor
 @Log4j2
+@Transactional
 public class AuthServiceImpl implements AuthService {
 
     private final MemberRepository memberRepository;
+    private final MemberService memberService;
     private final String KAKAO_GET_USER_INFO_URL = "https://kapi.kakao.com/v2/user/me";
 
     /*
@@ -36,43 +47,92 @@ public class AuthServiceImpl implements AuthService {
     @Override
     public PrincipalDetails kakaoLogin(String accessToken) {
         // accessToken을 이용해서 사용자 정보 가져오기
-        String email = getEmailFromKakaoAccessToken(accessToken);
+        LinkedHashMap<String, LinkedHashMap> kakaoAccount = getKakaoAccountFromKakao(accessToken);
+        String email = String.valueOf(kakaoAccount.get("email"));
         log.info("----------------------------------");
-        log.info("email : " +email);
+        log.info("email : " + email);
         // 기존 DB에 회원정보 조회
         Optional<Member> resultMember = memberRepository.findByEmail(email);
 
-        if(resultMember.isPresent()){
+        if (resultMember.isPresent()) {
             // 존재하면 대기/승인 상태 확인
-            if(resultMember.get().getStatus() == MemberStatus.APPROVED){
+            if (resultMember.get().getStatus() == MemberStatus.APPROVED) {
                 // 승인 회원이면 로그인 처리
                 PrincipalDetails existedDTO = entityToPrincipalDetails(resultMember.get());
                 return existedDTO;
-            }
-            else if(resultMember.get().getStatus() == MemberStatus.PENDING){
-                // 대기중이면 대기중 error
+            } else if (resultMember.get().getStatus() == MemberStatus.PENDING){
+            // 대기중이면 대기중 error
                 throw new CustomException(ErrorCode.ACCEPT_PENDING_USER);
             }
-        }
-        else if(!resultMember.isPresent()){
-            // 존재하지 않으면 회원가입 error
+        } else if (!resultMember.isPresent()){
+        // 존재하지 않으면 회원가입 error
             throw new CustomException(ErrorCode.SIGNUP_REQUIRED);
         }
         return null;
     }
 
     /*
-    * * accessToken으로 회원 카카오 계정 조회
-    *
-    * @Return 회원의 카카오 이메일 계정
-    */
+     * * 입력받은 회원 부가정보로 회원가입 처리
+     */
+    @Override
+    public response signup(String accessToken, request requestDTO) {
+        // accessToken으로 카카오 회원 정보 조회
+        LinkedHashMap<String, LinkedHashMap> kakaoAccount = getKakaoAccountFromKakao(accessToken);
 
-    private String getEmailFromKakaoAccessToken(String accessToken) {
+        Map<String, String> kakaoProfile = kakaoAccount.get("profile");
+        log.info("----------------------------------");
+        log.info("kakaoAccount : " + kakaoAccount);
+        log.info("kakaoProfile : " + kakaoProfile);
+
+        String email = String.valueOf(kakaoAccount.get("email"));
+        String profileURL = "";
+        if (kakaoProfile.get("is_default_image") == "false") {
+            profileURL = String.valueOf(kakaoAccount.get("profile_img_url"));
+        }
+        String nickname = String.valueOf(kakaoAccount.get("nickname"));
+
+
+        // 회원 정보 저장
+        Member newMember = makeNewMemberEntity(email, profileURL, nickname, requestDTO);
+
+        // 회원 프로필 mbti, 취미, 성격 저장
+        memberService.saveMemberProfiles(newMember, requestDTO);
+        Member saveMember = memberRepository.save(newMember);
+
+        // 이상형 프로필 저장
+        TypeProfiles typeProfiles = memberService.createTypeProfiles(saveMember, requestDTO);
+        memberService.saveTypeProfilesInfo(typeProfiles, requestDTO);
+
+        // 응답 DTO 반환
+        return response.builder()
+                .email(saveMember.getEmail())
+                .nickname(saveMember.getNickname())
+                .memberLocation(saveMember.getLocation())
+                .birthDate(saveMember.getBirthDate())
+                .gender(saveMember.getGender())
+                .bio(saveMember.getBio())
+                .memberMBTITag(saveMember.getMBTI())
+                .memberHobbyTags(saveMember.getMemberHobbies())
+                .memberPersonalityTags(saveMember.getMemberPersonalities())
+                .typeLocation(typeProfiles.getLocation())
+                .typeAgeMax(typeProfiles.getTypeAgeMax())
+                .typeAgeMin(typeProfiles.getTypeAgeMin())
+                .typeHobbyTags(typeProfiles.getTypeHobbies())
+                .typePersonalityTags(typeProfiles.getTypePersonalities())
+                .build();
+    }
+
+    /*
+     * * accessToken으로 카카오 회원정보 조회
+     *
+     * @Return 회원의 카카오 이메일 계정, 프로필사진url, 닉네임
+     * */
+    private LinkedHashMap<String, LinkedHashMap> getKakaoAccountFromKakao(String accessToken) {
         RestTemplate restTemplate = new RestTemplate();
 
         HttpHeaders headers = new HttpHeaders();
-        headers.add("Authorization","Bearer " + accessToken);
-        headers.add("Content-type","application/x-www-form-urlencoded;charset=utf-8");
+        headers.add("Authorization", "Bearer " + accessToken);
+        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
 
         HttpEntity<String> entity = new HttpEntity<>(headers);
 
@@ -83,11 +143,40 @@ public class AuthServiceImpl implements AuthService {
 
         LinkedHashMap<String, LinkedHashMap> bodyMap = responseKakaoUserInfo.getBody();
 
-        LinkedHashMap<String, String> kakaoAccount = bodyMap.get("kakao_account");
+        LinkedHashMap<String, LinkedHashMap> kakaoAccount = bodyMap.get("kakao_account");
+        log.info("// kakaoAccount : " + kakaoAccount);
+        return kakaoAccount;
+    }
 
-        String kakaoEmail = kakaoAccount.get("email");
-        log.info("kakaoEmail : " + kakaoEmail);
+    /*
+     * * 카카오 회원 계정 정보와 부가정보로 Member 엔티티 생성
+     *
+     * @param : 카카오 회원 계정 이메일, 프로필사진 url, 부가정보 DTO
+     * */
+    private Member makeNewMemberEntity(String email, String profileURL, String nickname,
+                                       MemberSignupDTO.request requestDTO) {
+        String tmpPW = makeTmpPW();
 
-        return kakaoEmail;
+        return Member.builder()
+                     .location(null)
+                     .role(MemberRole.USER)
+                     .email(email)
+                     .password(tmpPW)
+                     .nickname(nickname)
+                     .birthDate(LocalDate.parse(requestDTO.getBirthYear() + "0101", DateTimeFormatter.ofPattern("yyyyMMdd")))
+                     .gender(requestDTO.getGender())
+                     .bio(requestDTO.getBio())
+                     .profileImage(profileURL)
+                     .MBTI(null)
+                     .build();
+    }
+
+    private String makeTmpPW() {
+        StringBuffer buffer = new StringBuffer();
+
+        for (int i = 0; i < 10; i++) {
+            buffer.append((char) ((int) (Math.random() * 55) * 65));
+        }
+        return buffer.toString();
     }
 }
