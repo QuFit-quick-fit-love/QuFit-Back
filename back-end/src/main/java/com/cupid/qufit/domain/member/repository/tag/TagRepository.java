45e64d56f5f59f70de5904b046e7919188400b01
jhw <612gpdnjs@gmail.com>
2024-07-19 18:04:38 +0900

    #S11P12A209-29 #comment BE/ feat : 회원, 태그 관련 repository 추가

---
+++ back-end/src/main/java/com/cupid/qufit/domain/member/repository/tag/TagRepository.java
@@ -0,0 +1,8 @@
+package com.cupid.qufit.domain.member.repository.tag;
+
+import com.cupid.qufit.entity.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TagRepository extends JpaRepository<Tag, Long> {
+
+}
