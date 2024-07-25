5b768b212d4fdb8602ce32cafb04f93d082e000a
jhw <612gpdnjs@gmail.com>
2024-07-26 08:49:23 +0900

    #S11P12A209-133 #comment BE/ feat : dto 유효성 검사 exception 처리 추가

--- back-end/src/main/java/com/cupid/qufit/global/exception/ErrorCode.java
+++ back-end/src/main/java/com/cupid/qufit/global/exception/ErrorCode.java
@@ -15,9 +15,9 @@ public enum ErrorCode {
     MEMBER_DEFAULT_ERROR("회원 관련 오류가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
 	SIGNUP_FAILURE("회원가입 도중 오류가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
     USERNAME_ALREADY_EXISTS("이미 사용 중인 계정입니다.", HttpStatus.CONFLICT),
-    ACCEPT_PENDING_USER("가입 승인 대기 중인 계정입니다.", HttpStatus.NOT_FOUND),
+    ACCEPT_PENDING_USER("가입 승인 대기 중인 계정입니다.", HttpStatus.UNAUTHORIZED),
     MEMBER_NOT_FOUND("회원을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
-    SIGNUP_REQUIRED("회원 가입이 필요합니다.", HttpStatus.NOT_FOUND),
+    SIGNUP_REQUIRED("회원 가입이 필요합니다.", HttpStatus.UNAUTHORIZED),
 
     // 태그 관련
     TAG_NOT_FOUND("태그를 찾을 수 없습니다.",HttpStatus.NOT_FOUND),
--- back-end/src/main/java/com/cupid/qufit/global/exception/GlobalExceptionHandler.java
+++ back-end/src/main/java/com/cupid/qufit/global/exception/GlobalExceptionHandler.java
@@ -1,12 +1,17 @@
 package com.cupid.qufit.global.exception;
 
 
+import com.cupid.qufit.global.common.response.FieldValidationExceptionResponse;
 import com.cupid.qufit.global.exception.exceptionType.ChatException;
-import com.cupid.qufit.global.exception.exceptionType.CustomJWTException;
 import com.cupid.qufit.global.exception.exceptionType.MemberException;
 import com.cupid.qufit.global.exception.exceptionType.TagException;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -41,5 +46,23 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> handleException(Exception e) {
         log.error("예상치 못한 오류 : [UnexpectedException] : ", e);
         return ErrorResponse.toResponseEntity(ErrorCode.UNEXPECTED_ERROR);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<FieldValidationExceptionResponse>> handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException e) {
+        log.error("field validation error : [MethodArgumentNotValidException] : ", e);
+        List<FieldError> fieldErrors = e.getBindingResult().getFieldErrors();
+
+        List<FieldValidationExceptionResponse> errorResponse = fieldErrors.stream()
+                                                                          .map(error -> FieldValidationExceptionResponse.builder()
+                                                                                                                        .field(error.getField())
+                                                                                                                        .rejectedValue(
+                                                                                                                                error.getRejectedValue())
+                                                                                                                        .errorMessage(
+                                                                                                                                error.getDefaultMessage())
+                                                                                                                        .build())
+                                                                          .toList();
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 }
