d28b37ac7d41dfb3a93efbeaddac41722ddfc910
jhw <612gpdnjs@gmail.com>
2024-07-25 16:22:06 +0900

    #S11P12A209-132 #in-progress #comment BE/ feat : jwtcheckfilter 토큰 검사 하지않는 api 수정

--- back-end/src/main/java/com/cupid/qufit/global/security/filter/JWTCheckFilter.java
+++ back-end/src/main/java/com/cupid/qufit/global/security/filter/JWTCheckFilter.java
@@ -35,7 +35,7 @@ public class JWTCheckFilter extends OncePerRequestFilter {
     protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
 
         String path = request.getRequestURI();
-        if (path.startsWith("/qufit/auth")) {
+        if (path.startsWith("/Qufit/auth")) {
             return true; // 로그인, 회원가입 관련 api는 check하지 않음
         }
         return true;
