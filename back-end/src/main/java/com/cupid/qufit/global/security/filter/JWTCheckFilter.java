b5473e02d8741558f773d0aaebe6c40fc1afda8c
jhw <612gpdnjs@gmail.com>
2024-07-25 16:15:10 +0900

    #S11P12A209-132 #in-progress #comment BE/ refactor : jwtcheckfilter 코드 리팩토링

--- back-end/src/main/java/com/cupid/qufit/global/security/filter/JWTCheckFilter.java
+++ back-end/src/main/java/com/cupid/qufit/global/security/filter/JWTCheckFilter.java
@@ -4,13 +4,13 @@ import com.cupid.qufit.domain.member.dto.MemberDetails;
 import com.cupid.qufit.entity.MemberRole;
 import com.cupid.qufit.global.exception.ErrorCode;
 import com.cupid.qufit.global.exception.exceptionType.CustomJWTException;
+import com.cupid.qufit.global.redis.service.RedisRefreshTokenService;
 import com.cupid.qufit.global.security.util.JWTUtil;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.InvalidClassException;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -26,12 +26,16 @@ import org.springframework.web.filter.OncePerRequestFilter;
 public class JWTCheckFilter extends OncePerRequestFilter {
 
     private final JWTUtil jwtUtil;
+    private final RedisRefreshTokenService redisRefreshTokenService;
 
+    /*
+    * * 토큰 검증없이 접근 가능한 api를 설정함
+    * */
     @Override
     protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
 
         String path = request.getRequestURI();
-        if (path.startsWith("/auth")) {
+        if (path.startsWith("/qufit/auth")) {
             return true; // 로그인, 회원가입 관련 api는 check하지 않음
         }
         return false;
@@ -44,36 +48,73 @@ public class JWTCheckFilter extends OncePerRequestFilter {
 
         String accessToken = jwtUtil.getTokenFromHeader(request);
 
-        // AccessToken 만료여부 확인
+        // AccessToken 유효성 검사
         if (!jwtUtil.checkTokenExpired(accessToken)) {
-            // AccessToken 유효하면 Authentication 설정
+            // AccessToken 유효
+            log.info("[accessToken is valid]");
             setAuthentication(accessToken);
         } else {
-            // TODO : AccessToken 만료되었으면  refreshToken 확인
+            log.info("[accessToken is invalid]");
+            // AccessToken 만료 -> RefreshToken 확인
+            String refreshToken = redisRefreshTokenService.getRedisDataByAccessToken(accessToken);
+
+            if (refreshToken != null && !jwtUtil.checkTokenExpired(refreshToken)) {
+                // RefreshToken 유효
+                handleExpiredAccessToken(response, refreshToken);
+            } else {
+                // RefreshToken 만료
+                log.error("[refresh token is expired]");
+                throw new CustomJWTException(ErrorCode.EXPIRED_TOKEN);
+            }
         }
         filterChain.doFilter(request, response);
     }
 
-    private void setAuthentication(String accessToken) {
-        try {
-            log.info("--------setAuthentication-----");
-            Map<String, Object> claims = jwtUtil.validateToken(accessToken);
-            log.info("claims : " + claims);
+    /*
+    * * Access Token이 만료되고, RefreshToken은 유효할 경우 실행될 메소드
+    *
+    * - Access Token이 새로 발급되고 response header에 저장됨
+    * - Refresh Token의 유효시간이 1시간 남았다면 다시 Refresh Token을 발급해서 Redis에 저장함
+    * */
+    private void handleExpiredAccessToken(HttpServletResponse response, String refreshToken) {
+        // refreshToken 유효 -> 새로운 accessToken 발급
+        Map<String, Object> claims = jwtUtil.validateToken(refreshToken);
+        Long memberId = ((Number) claims.get("id")).longValue();
 
-            Long id = ((Number) claims.get("id")).longValue();
-            String email = (String) claims.get("email");
-            MemberRole role = claims.get("role").equals("USER") ? MemberRole.USER : MemberRole.ADMIN;
+        String newAccessToken = jwtUtil.generateToken(claims, "access");
+        log.info("[new access token generate] ");
+        setAuthentication(newAccessToken);
+        jwtUtil.setTokenToHeader(response, newAccessToken);
 
-            MemberDetails memberDetails = new MemberDetails(id, email, role);
-            log.info("memberROLE : " + memberDetails.getAuthorities());
+        // refreshToken 유효시간 1시간 남았다면 새로운 refreshToken 발급
+        Integer refreshTokenExp = (Integer) claims.get("exp");
 
-            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
-                    memberDetails, "", memberDetails.getAuthorities());
-
-            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-
-        } catch (InvalidClassException e) {
-            throw new RuntimeException(e);
+        if (jwtUtil.checkTokenExpiringInAnHour(refreshTokenExp)) {
+            String newRefreshToken = jwtUtil.generateToken(claims, "refresh");
+            redisRefreshTokenService.saveRedisData(memberId, newRefreshToken, newAccessToken);
+            log.info("[new refresh token generate] ");
+        } else {
+            redisRefreshTokenService.saveRedisData(memberId, refreshToken, newAccessToken);
         }
+    }
+
+    /*
+    * * Security Context Holder 안에 Authentication을 저장함
+    * */
+    private void setAuthentication(String accessToken) {
+        log.info("--------setAuthentication-----");
+        Map<String, Object> claims = jwtUtil.validateToken(accessToken);
+        log.info("claims : " + claims);
+
+        Long id = ((Number) claims.get("id")).longValue();
+        String email = (String) claims.get("email");
+        MemberRole role = claims.get("role").equals("USER") ? MemberRole.USER : MemberRole.ADMIN;
+
+        MemberDetails memberDetails = new MemberDetails(id, email, role);
+
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                memberDetails, "", memberDetails.getAuthorities());
+
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
     }
 }
