aa4437c2ef52e399a2fe364fab9fd14f84681ff4
hyunsoo730 <whgustn730@gmail.com>
2024-07-25 15:55:48 +0900

    S11P12A209-138 #comment Swagger 구성 완료: group 설정 및 API 추가

--- back-end/src/main/java/com/cupid/qufit/domain/chat/dto/ChatRoomDTO.java
+++ back-end/src/main/java/com/cupid/qufit/domain/chat/dto/ChatRoomDTO.java
@@ -4,6 +4,7 @@ import com.cupid.qufit.entity.Member;
 import com.cupid.qufit.entity.chat.ChatMessage;
 import com.cupid.qufit.entity.chat.ChatRoom;
 import com.cupid.qufit.entity.chat.ChatRoomMember;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,13 +17,21 @@ import lombok.NoArgsConstructor;
 @Builder
 public class ChatRoomDTO {
 
+    @Schema(description = "채팅방ID")
     private Long chatRoomId;
+    @Schema(description = "상대방 회원의 ID")
     private Long otherMemberId;
+    @Schema(description = "상대방 닉네임")
     private String otherMemberNickname;
+    @Schema(description = "마지막 메시지")
     private String lastMessage;
+    @Schema(description = "마지막 메시지ID")
     private String lastMessageId;
+    @Schema(description = "마지막 메시지 보낸 시간")
     private LocalDateTime lastMessageTime;
+    @Schema(description = "안 읽은 메시지 개수")
     private int unreadCount;
+    @Schema(description = "마지막으로 읽은 메시지ID")
     private String lastReadMessageId; // ! 가장 마지막으로 읽은 메세지 ID
 
     public static ChatRoomDTO from(ChatRoom chatRoom, ChatRoomMember currentMember, Member otherMember) {
