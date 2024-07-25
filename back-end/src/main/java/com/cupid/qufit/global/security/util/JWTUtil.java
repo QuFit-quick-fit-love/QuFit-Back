390645b72e631cac2f5693d542774cf86195bca5
jhw <612gpdnjs@gmail.com>
2024-07-25 16:17:04 +0900

    #S11P12A209-132 #in-progress #comment BE/ feat : jwtUtil 토큰 유효성 검사 추가 및 수정

--- back-end/src/main/java/com/cupid/qufit/global/security/util/JWTUtil.java
+++ back-end/src/main/java/com/cupid/qufit/global/security/util/JWTUtil.java
@@ -3,6 +3,7 @@ package com.cupid.qufit.global.security.util;
 import com.cupid.qufit.global.exception.ErrorCode;
 import com.cupid.qufit.global.exception.exceptionType.CustomJWTException;
 import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.InvalidClaimException;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.MalformedJwtException;
@@ -10,6 +11,7 @@ import io.jsonwebtoken.UnsupportedJwtException;
 import io.jsonwebtoken.security.Keys;
 import io.jsonwebtoken.security.SecurityException;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.InvalidClassException;
 import java.io.UnsupportedEncodingException;
 import java.time.ZonedDateTime;
@@ -26,14 +28,16 @@ import org.springframework.stereotype.Component;
 @Log4j2
 public class JWTUtil {
 
+    @Value("${ACCESS_TOKEN_EXPIRATION_PERIOD}")
+    private Long ACCESS_TOKEN_EXPIRATION_PERIOD;
+    @Value("${REFRESH_TOKEN_EXPIRATION_PERIOD}")
+    private Long REFRESH_TOKEN_EXPIRATION_PERIOD;
     @Value("${JWT_KEY}")
     private String JWT_KEY;
-    private final Long ACCESS_TOKEN_EXPIRATION_PERIOD = 60L; // accesstoken 60분(임의 설정)
-    private final Long REFRESH_TOKEN_EXPIRATION_PERIOD = 60 * 24L; // refreshtoken 1일(임의 설정)
 
     /*
-    * 토큰 생성
-    * */
+     * 토큰 생성
+     * */
     public String generateToken(Map<String, Object> valueMap, String type) {
         log.info("---------createToken---------");
         SecretKey key = null;
@@ -44,7 +48,9 @@ public class JWTUtil {
         }
 
         Long expirationPeriod = ACCESS_TOKEN_EXPIRATION_PERIOD;
-        if (type.equals("refresh")) expirationPeriod = REFRESH_TOKEN_EXPIRATION_PERIOD;
+        if (type.equals("refresh")) {
+            expirationPeriod = REFRESH_TOKEN_EXPIRATION_PERIOD;
+        }
 
         String jwtStr = Jwts.builder()
                             .setHeader(Map.of("type", "JWT"))
@@ -58,9 +64,11 @@ public class JWTUtil {
     }
 
     /*
-    * 토큰 유효성 검사
-    * */
-    public Map<String, Object> validateToken(String token) throws JwtException, InvalidClassException {
+     * 토큰 유효성 검사 (expired 제외)
+     *
+     * - 토큰의 유효기간 만료는 다른 메소드로 관리함
+     * */
+    public Map<String, Object> validateToken(String token) {
 
         Map<String, Object> claim = null;
 
@@ -71,13 +79,16 @@ public class JWTUtil {
                         .setSigningKey(key)
                         .parseClaimsJws(token) // 파싱 및 검증, 실패 시 에러
                         .getBody();
-
-        } catch (SecurityException | MalformedJwtException e){
+        }catch (ExpiredJwtException e){
+            throw e;
+        } catch (MalformedJwtException e) {
             throw new CustomJWTException(ErrorCode.MALFORMED_TOKEN);
-        } catch (ExpiredJwtException e) {
-            throw new CustomJWTException(ErrorCode.EXPIRED_TOKEN);
-        } catch (UnsupportedJwtException e){
+        } catch (UnsupportedJwtException e) {
             throw new CustomJWTException(ErrorCode.UNSUPPORTED_TOKEN);
+        } catch (InvalidClaimException invalidClaimException) {
+            throw new CustomJWTException(ErrorCode.INVALID_TOKEN);
+        } catch (UnsupportedEncodingException e) {
+            throw new CustomJWTException(ErrorCode.UNSUPPORTED_ENCODING);
         } catch (Exception e) {
             throw new CustomJWTException(ErrorCode.TOKEN_DEFAULT_ERROR);
         }
@@ -87,7 +98,7 @@ public class JWTUtil {
     /*
      * 요청 헤더에서 accesstoken 가져옴
      * */
-    public String getTokenFromHeader(HttpServletRequest request){
+    public String getTokenFromHeader(HttpServletRequest request) {
         String authHeaderStr = request.getHeader("Authorization");
 
         // access token : Bearer(7자) + JWT 문자열
@@ -98,18 +109,34 @@ public class JWTUtil {
     }
 
     /*
-    * * 토큰 만료되었는지 확인
+    * * 응답 헤더에 accesstoken 저장
     * */
+    public void setTokenToHeader(HttpServletResponse response, String accessToken){
+        response.setHeader("Authorization", "Bearer " + accessToken);
+    }
+
+    /*
+     * * 토큰 만료되었는지 확인
+     * */
     public boolean checkTokenExpired(String token) {
         try {
             validateToken(token);
-        } catch (CustomJWTException e) {
-            if (e.getErrorCode() == ErrorCode.EXPIRED_TOKEN) {
-                return true;
-            }
-        } catch (InvalidClassException e) {
-            throw new RuntimeException(e);
+        } catch (ExpiredJwtException e) {
+            return true;
         }
         return false;
+    }
+
+    /*
+     * * 토큰 유효시간 1시간 미만인지 확인
+     * */
+    public boolean checkTokenExpiringInAnHour(Integer exp) {
+        Date expDate = new Date((long) exp * 1000);
+
+        // 현재 시간과의 차이 계산 (min)
+        long gap = (expDate.getTime() - System.currentTimeMillis()) / (1000 * 60);
+
+        // 1시간 남았는지 확인
+        return gap < 60;
     }
 }
