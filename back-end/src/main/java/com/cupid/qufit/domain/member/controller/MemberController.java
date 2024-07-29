1c0d2ab46afd46cfb40893f68570689c599ae824
jhw <612gpdnjs@gmail.com>
2024-07-29 13:48:29 +0900

    #S11P12A209-169 #comment BE feat : 회원 관련 api swagger 설정 추가

--- back-end/src/main/java/com/cupid/qufit/domain/member/controller/MemberController.java
+++ back-end/src/main/java/com/cupid/qufit/domain/member/controller/MemberController.java
@@ -5,7 +5,13 @@ import com.cupid.qufit.domain.member.dto.MemberInfoDTO;
 import com.cupid.qufit.domain.member.service.MemberService;
 import com.cupid.qufit.entity.Member;
 import com.cupid.qufit.entity.MemberStatus;
+import com.cupid.qufit.entity.chat.ChatRoom;
 import com.cupid.qufit.global.common.response.CommonResultResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -34,6 +40,10 @@ public class MemberController {
      * * 회원 정보 조회
      * */
     @GetMapping
+    @Operation(summary = "회원 정보 조회", description = "회원 프로필을 조회한다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "회원 정보 조회 성공")
+    })
     public ResponseEntity<?> getMemberInfo(@AuthenticationPrincipal MemberDetails memberDetails) {
         Long currentMemberId = memberDetails.getId();
         MemberInfoDTO.Response response = memberService.getMemberInfo(currentMemberId);
@@ -44,6 +54,11 @@ public class MemberController {
      * * 회원 정보 수정
      * */
     @PutMapping
+    @Operation(summary = "회원 정보 수정", description = "회원 프로필을 수정한 후 조회한다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "회원 정보 수정 성공"),
+            @ApiResponse(responseCode = "400", description = "요청 dto 필드값 오류")
+    })
     public ResponseEntity<?> updateMemberInfo(@Valid @RequestBody MemberInfoDTO.Request request,
                                               @AuthenticationPrincipal MemberDetails memberDetails) {
         Long currentMemberId = memberDetails.getId();
@@ -55,6 +70,12 @@ public class MemberController {
      * * 회원 탈퇴
      * */
     @DeleteMapping
+    @Operation(summary = "회원 탈퇴", description = "회원을 탈퇴 처리한다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "회원 탈퇴 처리 성공"),
+            @ApiResponse(responseCode = "400", description = "MEMBER_ALREADY_WITHDRAWN : 이미 탈퇴한 회원"),
+            @ApiResponse(responseCode = "500", description = "탈퇴 중 서버 오류")
+    })
     public ResponseEntity<?> deleteMember(@AuthenticationPrincipal MemberDetails memberDetails) {
         Long currentMemberId = memberDetails.getId();
 
@@ -65,7 +86,7 @@ public class MemberController {
                                                                 .isSuccess(false)
                                                                 .message("탈퇴 처리 되지 않았습니다.")
                                                                 .build();
-            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
         CommonResultResponse response = CommonResultResponse.builder()
