b079f21ac577833e1971df059431bc18d900269a
김해엽 <rjssod2@naver.com>
2024-07-29 09:15:28 +0900

    #S11P12A209-111 #in-progress #comment BE feat: 미팅룸 생성 및 참여 기능 구현

--- back-end/src/main/java/com/cupid/qufit/domain/video/controller/VideoRoomController.java
+++ back-end/src/main/java/com/cupid/qufit/domain/video/controller/VideoRoomController.java
@@ -1,18 +1,14 @@
 package com.cupid.qufit.domain.video.controller;
 
-import io.livekit.server.AccessToken;
-import io.livekit.server.RoomJoin;
-import io.livekit.server.RoomName;
-import io.livekit.server.WebhookReceiver;
+import com.cupid.qufit.domain.video.dto.VideoRoomRequest;
+import com.cupid.qufit.domain.video.service.VideoRoomService;
 import java.util.Map;
-import livekit.LivekitWebhook.WebhookEvent;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,49 +17,61 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class VideoRoomController {
 
-    @Value("${livekit.api.key}")
-    private String LIVEKIT_API_KEY;
-
-    @Value("${livekit.api.secret}")
-    private String LIVEKIT_API_SECRET;
+    private final VideoRoomService videoRoomService;
+//    /**
+//     *
+//     * 방 제목, 참가자 이름을 받아 방에 연결해준다.
+//     * 응답 : token
+//     */
+//    @CrossOrigin(origins = "http://localhost:5080")
+//    @PostMapping(value = "/token")
+//    public ResponseEntity<Map<String, String>> createToken(@RequestBody Map<String, String> params) {
+//        String roomName = params.get("roomName");
+//        String participantName = params.get("participantName");
+//
+//        if (roomName == null || participantName == null) {
+//            return ResponseEntity.badRequest()
+//                    .body(Map.of("errorMessage", "roomName and participantName are required"));
+//        }
+//
+//        AccessToken token = new AccessToken(LIVEKIT_API_KEY, LIVEKIT_API_SECRET);
+//        token.setName(participantName);
+//        token.setIdentity(participantName);
+//        token.addGrants(new RoomJoin(true), new RoomName(roomName));
+//
+//        return ResponseEntity.ok(Map.of("token", token.toJwt()));
+//    }
+//
+//    /**
+//     * 이벤트 수신용
+//     */
+//    @PostMapping(value = "/livekit/webhook", consumes = "application/webhook+json")
+//    public ResponseEntity<String> receiveWebhook(@RequestHeader("Authorization") String authHeader, @RequestBody String body) {
+//        WebhookReceiver webhookReceiver = new WebhookReceiver(LIVEKIT_API_KEY, LIVEKIT_API_SECRET);
+//        try {
+//            WebhookEvent event = webhookReceiver.receive(body, authHeader);
+//            System.out.println("LiveKit Webhook: " + event.toString());
+//        } catch (Exception e) {
+//            System.err.println("Error validating webhook event: " + e.getMessage());
+//        }
+//        return ResponseEntity.ok("ok");
+//    }
 
     /**
-     *
-     * 방 제목, 참가자 이름을 받아 방에 연결해준다.
-     * 응답 : token
+     * 미팅룸 생성
      */
-    @CrossOrigin(origins = "http://localhost:5080")
-    @PostMapping(value = "/token")
-    public ResponseEntity<Map<String, String>> createToken(@RequestBody Map<String, String> params) {
-        String roomName = params.get("roomName");
-        String participantName = params.get("participantName");
-
-        if (roomName == null || participantName == null) {
-            return ResponseEntity.badRequest()
-                                 .body(Map.of("errorMessage", "roomName and participantName are required"));
-        }
-
-        AccessToken token = new AccessToken(LIVEKIT_API_KEY, LIVEKIT_API_SECRET);
-        token.setName(participantName);
-        token.setIdentity(participantName);
-        token.addGrants(new RoomJoin(true), new RoomName(roomName));
-
-        return ResponseEntity.ok(Map.of("token", token.toJwt()));
+    @PostMapping
+    public ResponseEntity<?> createVideoRoom(@RequestBody VideoRoomRequest videoRoomRequest) {
+        return new ResponseEntity<>(videoRoomService.createVideoRoom(videoRoomRequest), HttpStatus.OK);
     }
 
     /**
-     * 이벤트 수신용
+     * 방 참가
      */
-    @PostMapping(value = "/livekit/webhook", consumes = "application/webhook+json")
-    public ResponseEntity<String> receiveWebhook(@RequestHeader("Authorization") String authHeader, @RequestBody String body) {
-        WebhookReceiver webhookReceiver = new WebhookReceiver(LIVEKIT_API_KEY, LIVEKIT_API_SECRET);
-        try {
-            WebhookEvent event = webhookReceiver.receive(body, authHeader);
-            System.out.println("LiveKit Webhook: " + event.toString());
-        } catch (Exception e) {
-            System.err.println("Error validating webhook event: " + e.getMessage());
-        }
-        return ResponseEntity.ok("ok");
+    @PostMapping("{videoRoomId}/join")
+    public ResponseEntity<?> joinVideoRoom(@PathVariable Long videoRoomId,
+                                           @RequestBody VideoRoomRequest videoRoomRequest) {
+        return new ResponseEntity<>(Map.of("token", videoRoomService.joinVideoRoom(videoRoomId, videoRoomRequest)),
+                HttpStatus.OK);
     }
-
 }
--- back-end/src/main/java/com/cupid/qufit/domain/video/service/VideoRoomService.java
+++ back-end/src/main/java/com/cupid/qufit/domain/video/service/VideoRoomService.java
@@ -1,5 +1,11 @@
 package com.cupid.qufit.domain.video.service;
 
+import com.cupid.qufit.domain.video.dto.VideoRoomRequest;
+import com.cupid.qufit.domain.video.dto.VideoRoomResponse;
+
 public interface VideoRoomService {
 
+    VideoRoomResponse createVideoRoom(VideoRoomRequest videoRoomRequset);
+
+    String joinVideoRoom(Long videoRoomId, VideoRoomRequest videoRoomRequset);
 }
