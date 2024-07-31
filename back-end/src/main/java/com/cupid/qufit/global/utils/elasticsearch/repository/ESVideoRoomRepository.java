17339843c9938ebdd3f0234751d9e8d09e2bebca
hyunsoo730 <whgustn730@gmail.com>
2024-07-31 15:44:09 +0900

    S11P12A209-191 #in-progress ES 주석 & ErrorCde 수정

--- back-end/src/main/java/com/cupid/qufit/global/utils/elasticsearch/repository/ESVideoRoomRepository.java
+++ back-end/src/main/java/com/cupid/qufit/global/utils/elasticsearch/repository/ESVideoRoomRepository.java
@@ -1,8 +1,8 @@
-package com.cupid.qufit.global.utils.elasticsearch.repository;
-
-import com.cupid.qufit.global.utils.elasticsearch.entity.ESVideoRoom;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-public interface ESVideoRoomRepository extends ElasticsearchRepository<ESVideoRoom, Long> {
-
-}
+//package com.cupid.qufit.global.utils.elasticsearch.repository;
+//
+//import com.cupid.qufit.global.utils.elasticsearch.entity.ESVideoRoom;
+//import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+//
+//public interface ESVideoRoomRepository extends ElasticsearchRepository<ESVideoRoom, Long> {
+//
+//}
