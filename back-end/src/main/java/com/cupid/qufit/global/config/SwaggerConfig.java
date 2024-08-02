5bb76847dec04f1c4f4e2ee67e35c086f860450e
hyunsoo730 <whgustn730@gmail.com>
2024-08-02 12:35:42 +0900

    S11P12A209-194 #in-progress #comment feat/BE : Swagger JWT 인증 추가

--- back-end/src/main/java/com/cupid/qufit/global/config/SwaggerConfig.java
+++ back-end/src/main/java/com/cupid/qufit/global/config/SwaggerConfig.java
@@ -4,6 +4,9 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import java.util.Arrays;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -40,8 +43,20 @@ public class SwaggerConfig {
                                  .url("http://i11a209.p.ssafy.io:8080")
                 );
 
+        // JWT 보안 스키마 정의
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization");
+
+        // 전역 보안 요구사항 정의
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
+
         return new OpenAPI()
-                .components(new Components())
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+                .security(Arrays.asList(securityRequirement))
                 .info(info);
     }
 
