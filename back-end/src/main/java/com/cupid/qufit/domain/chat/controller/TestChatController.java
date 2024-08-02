e12b83d4731c38be21ddee8d5fa79fb4a67b159c
hyunsoo730 <whgustn730@gmail.com>
2024-08-02 12:35:09 +0900

    S11P12A209-194 #in-progress #comment feat/BE : TestController 채팅방 나가기 테스트 추가

--- back-end/src/main/java/com/cupid/qufit/domain/chat/controller/TestChatController.java
+++ back-end/src/main/java/com/cupid/qufit/domain/chat/controller/TestChatController.java
@@ -1,8 +1,10 @@
 package com.cupid.qufit.domain.chat.controller;
 
+import com.cupid.qufit.domain.chat.dto.ChatMessageDTO;
 import com.cupid.qufit.domain.chat.dto.ChatRoomMessageResponse;
 import com.cupid.qufit.domain.chat.service.ChatService;
 import com.cupid.qufit.domain.member.dto.MemberDetails;
+import com.cupid.qufit.domain.member.dto.MemberInfoDTO.Response;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -10,8 +12,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import retrofit2.http.Path;
 
 @RestController
 @RequiredArgsConstructor
@@ -28,5 +33,15 @@ public class TestChatController {
 
         ChatRoomMessageResponse response = chatService.enterChatRoom(chatRoomId, memberId, 20);
         return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @PostMapping("/leave/{chatRoomId}")
+    public ResponseEntity<?> leaveRoom(@AuthenticationPrincipal MemberDetails memberDetails,
+                                       @RequestBody ChatMessageDTO lastMessage,
+                                       @PathVariable("chatRoomId") Long chatRoomId) {
+        Long memberId = memberDetails.getId();
+        chatService.updateChatRoomMember(chatRoomId, memberId, lastMessage);
+
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
