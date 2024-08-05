bb99aaa6ec922bb21ab85da4e83fe5514f92ce5b
천세경 <jay45652@gmail.com>
2024-08-05 10:14:07 +0900

    #S11P12A209-152 BE refactor: VideoRoomController의 요청에 맞게 구조 변경

--- back-end/src/main/java/com/cupid/qufit/global/utils/elasticsearch/controller/ESParticipantController.java
+++ back-end/src/main/java/com/cupid/qufit/global/utils/elasticsearch/controller/ESParticipantController.java
@@ -1,10 +1,14 @@
 package com.cupid.qufit.global.utils.elasticsearch.controller;
 
+import com.cupid.qufit.global.utils.elasticsearch.dto.RecommendRoomDTO;
 import com.cupid.qufit.global.utils.elasticsearch.entity.ESParticipant;
-import com.cupid.qufit.global.utils.elasticsearch.service.ESParticipantService;
+import com.cupid.qufit.global.utils.elasticsearch.service.ESParticipantServiceImpl;
 import jakarta.validation.Valid;
+import java.io.IOException;
 import java.net.URI;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,23 +22,24 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 @RestController
 @RequestMapping("/qufit/es-participant")
 @RequiredArgsConstructor
+@Slf4j
 public class ESParticipantController {
 
-    private final ESParticipantService esParticipantService;
+    private final ESParticipantServiceImpl esParticipantServiceImpl;
 
     @GetMapping("/{id}")
     public ResponseEntity<ESParticipant> getVideoRoomById(@PathVariable("id") Long id) {
-        return ResponseEntity.ok().body(esParticipantService.findById(id));
+        return ResponseEntity.ok().body(esParticipantServiceImpl.findById(id));
     }
 
     @GetMapping
     public ResponseEntity<Iterable<ESParticipant>> getAllVideoRooms() {
-        return ResponseEntity.ok().body(esParticipantService.findAll());
+        return ResponseEntity.ok().body(esParticipantServiceImpl.findAll());
     }
 
     @PostMapping
     public ResponseEntity<ESParticipant> createVideoRoom(@RequestBody @Valid ESParticipant esParticipant) {
-        ESParticipant createdParticipant = esParticipantService.save(esParticipant);
+        ESParticipant createdParticipant = esParticipantServiceImpl.save(esParticipant);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
                                              .path("/{id}")
                                              .buildAndExpand(createdParticipant.getId())
@@ -44,14 +49,29 @@ public class ESParticipantController {
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteVideoRoomById(@PathVariable("id") Long id) {
-        esParticipantService.deleteById(id);
+        esParticipantServiceImpl.deleteById(id);
         return ResponseEntity.noContent().build();
     }
 
     @DeleteMapping
     public void deleteAllVideoRooms() {
-        esParticipantService.deleteAll();
+        esParticipantServiceImpl.deleteAll();
     }
 
+    @GetMapping("/search")
+    public void search() throws IOException {
+        log.info("search");
+        RecommendRoomDTO.Request dummyRequest = RecommendRoomDTO.Request.builder()
+                                                                        .location("서울")
+                                                                        .birthYear(1995)
+                                                                        .typeAgeMax(35)
+                                                                        .typeAgeMin(25)
+                                                                        .MBTIs(List.of("INFJ", "INFP", "INTP"))
+                                                                        .personalities(List.of("예술적", "이해심"))
+                                                                        .hobbies(List.of("그림 그리기", "명상"))
+                                                                        .gender("F")
+                                                                        .build();
+        esParticipantServiceImpl.recommendRoom(dummyRequest);
+    }
 
 }
