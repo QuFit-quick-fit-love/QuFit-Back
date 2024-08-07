bc8ff5ba413349c77d78d7ba27de2b502a1c5ff9
김해엽 <rjssod2@naver.com>
2024-08-07 13:39:54 +0900

    #S11P12A209-225 #in-progress #comment BE add : 비디오 방 상태 entity 필드 추가 및 에러 코드 추가 - PERSONAL 일대일 방 추가 - 방 상태 입력 에러 코드 추가

--- back-end/src/main/java/com/cupid/qufit/global/exception/ErrorCode.java
+++ back-end/src/main/java/com/cupid/qufit/global/exception/ErrorCode.java
@@ -60,6 +60,7 @@ public enum ErrorCode {
     HOST_NOT_FOUND("방장을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
     NOT_ROOM_HOST("방장이 아닙니다.", HttpStatus.FORBIDDEN),
     PARTICIPANT_COUNT_MISMATCH("현재 남자, 여자 인원이 일치하지 않습니다.", HttpStatus.BAD_REQUEST),
+    INVALID_STATUS_TYPE("유효하지 않은 방 상태입니다.", HttpStatus.BAD_REQUEST),
 
     // 친구 관련
     FRIEND_ALREADY_EXISTS("이미 해당 멤버와 친구 관계입니다.", HttpStatus.CONFLICT),
