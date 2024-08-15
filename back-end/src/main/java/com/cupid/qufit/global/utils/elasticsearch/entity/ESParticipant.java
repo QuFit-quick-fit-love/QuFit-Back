fd7b527eba3382ba0e21c18460c568892127a316
김해엽 <rjssod2@naver.com>
2024-08-15 21:53:41 +0900

    fix: ES id 필드 설정 - id 필드 멤버 id와 동일

--- back-end/src/main/java/com/cupid/qufit/global/utils/elasticsearch/entity/ESParticipant.java
+++ back-end/src/main/java/com/cupid/qufit/global/utils/elasticsearch/entity/ESParticipant.java
@@ -2,8 +2,10 @@ package com.cupid.qufit.global.utils.elasticsearch.entity;
 
 import com.cupid.qufit.entity.Member;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import java.util.List;
 import java.util.stream.Collectors;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -60,19 +62,20 @@ public class ESParticipant {
 
     public static ESParticipant createParticipant(String videoRoomId, Member member) {
         return ESParticipant.builder()
-                            .participantId(member.getId().toString())
-                            .videoRoomId(videoRoomId)
-                            .MBTI(member.getMBTI().getTagName())
-                            .personalities(member.getMemberPersonalities().stream()
-                                                 .map(pTag -> pTag.getTag().getTagName())
-                                                 .collect(Collectors.toList()))
-                            .hobbies(member.getMemberHobbies().stream()
-                                           .map(hTag -> hTag.getTag().getTagName())
-                                           .collect(Collectors.toList()))
-                            .location(member.getLocation().getSi())
-                            .birthYear(member.getBirthDate().getYear())
-                            .gender(member.getGender().toString())
-                            .bio(member.getBio())
-                            .build();
+                .id(member.getId().toString())
+                .participantId(member.getId().toString())
+                .videoRoomId(videoRoomId)
+                .MBTI(member.getMBTI().getTagName())
+                .personalities(member.getMemberPersonalities().stream()
+                        .map(pTag -> pTag.getTag().getTagName())
+                        .collect(Collectors.toList()))
+                .hobbies(member.getMemberHobbies().stream()
+                        .map(hTag -> hTag.getTag().getTagName())
+                        .collect(Collectors.toList()))
+                .location(member.getLocation().getSi())
+                .birthYear(member.getBirthDate().getYear())
+                .gender(member.getGender().toString())
+                .bio(member.getBio())
+                .build();
     }
 }
